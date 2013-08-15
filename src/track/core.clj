38eef3d85bb2129937e452147214b3222a4db5fa
Dmitry Kasimtsev <dmitry.kasimtsev@strikead.com>
2013-08-15 13:25:58 +0300

    click method draft implementation

--- src/track/core.clj
+++ src/track/core.clj
@@ -6,11 +6,20 @@
   (:require 
     [compojure.route :as route]
     [ring.util.response :as response]))
-
+; HANDLERS
 (defn pixel "doc-string" [request]
   (response/redirect "http://google.com"))
 
+(defn click "redirects if landing presented" [request]
+  (if-let [landing (:landing request)]
+    (response/redirect landing)
+    (response/status (response/response "landing requred") 400)))
+; /click?landing=
+; /pixel
+; /pixel.png
+; ? /pixel.js
 (defroutes  all-routes
+  (GET "/click" [] click)
   (GET "/pixel" [] pixel)
   (route/not-found "<p>Page not found</p>"))
 
